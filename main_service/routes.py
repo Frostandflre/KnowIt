7ab0aed6ebfa5bfd333a94ff352920b001cf2466
Vadim <mineciler555@gmail.com>
2025-11-01 23:18:04 +0300

    приветственная страница,доработка дизайна,компоновка пусковых скриптов

--- main_service/routes.py
+++ main_service/routes.py
@@ -6,8 +6,8 @@ API_GATEWAY_URL = "http://localhost:5005"
 
 @main.route("/")
 def main_page():
-    nickname = "Guest"
     if 'nickname' in request.cookies:
         nickname = request.cookies.get('nickname')
+        return render_template("main_page.html", nickname=nickname)
 
-    return render_template("main_page.html",nickname = nickname)
+    return render_template("welcome_page.html",nickname="Guest")
