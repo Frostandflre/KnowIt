bdbf8e805aa0d4a9bf25f2f77fa367da3d7027a0
Frostandflre <mineciler555@gmail.com>
2026-06-17 10:20:14 +0300

    доработка запуска сайта

--- entrypoints/run_main_service.py
+++ entrypoints/run_main_service.py
@@ -1,6 +1,9 @@
 from main_service import create_main_app
+import os
 
 main_app = create_main_app()
 
 if __name__ == "__main__":
-    main_app.run(port=5000)
+    port = int(os.environ.get("PORT", 5000))
+
+    main_app.run(host="0.0.0.0", port=port, debug=False)
